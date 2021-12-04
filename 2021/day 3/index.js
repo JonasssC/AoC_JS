ee8a51e3a54eeeaf1cf05b0c5f8efd4d0975b0a7
JonasssC <jonas.couwberghs@student.kuleuven.be>
2021-12-04 03:16:11 +0100

    cleanup 2021 day 3

--- 2021/day 3/index.js
+++ 2021/day 3/index.js
@@ -6,63 +6,53 @@ let lines = input
     .split('\r\n')
     .filter(line => line !== "");
 
-let count = new Array(lines[0].length).fill(0);
+let count = countOccurenceOf1PerPosition(lines);
 
-for(let line of lines) {
-    for(let i = 0; i < line.length; i++) {
-        if(line[i] === '1') count[i]++;
-    }
-}
-
-let gammaBin = count.map(c => c > lines.length / 2 ? '1' : '0').join('');
-let epsilonBin = count.map(c => c > lines.length / 2 ? '0' : '1').join('');
-
-let gamma = parseInt(gammaBin, 2);
-let epsilon = parseInt(epsilonBin, 2);
+let gamma = parseInt(calcGammaBin(lines, count), 2);
+let epsilon = parseInt(calcEpsilonBin(lines, count), 2);
 
 console.log("Part 1: ", gamma * epsilon);
 
+function countOccurenceOf1PerPosition(items) {
+    let count = new Array(items[0].length).fill(0);
+
+    for (let item of items)
+        for (let i = 0; i < item.length; i++)
+            if(item[i] === '1') count[i]++;
+
+    return count;
+}
+
+function calcGammaBin(items, count = countOccurenceOf1PerPosition(items)) {
+    return count.map(c => c >= items.length / 2 ? '1' : '0').join('');
+}
+
+function calcEpsilonBin(items, count = countOccurenceOf1PerPosition(items)) {
+    return count.map(c => c >= items.length / 2 ? '0' : '1').join('');
+}
+
 // Part 2
 
-let numbers = lines.slice();
-
-for(let i = 0; i < count.length; i++) {
-    count = new Array(lines[0].length).fill(0);
-
-    for(let line of numbers) {
-        for(let i = 0; i < line.length; i++) {
-            if(line[i] === '1') count[i]++;
-        }
-    }
-
-    gammaBin = count.map(c => c >= numbers.length / 2 ? '1' : '0').join('');
-
-    numbers = numbers.filter(value => value[i] === gammaBin[i]);
-}
-
-let oxygenBin = numbers[0];
-
-numbers = lines.slice();
-
-for(let i = 0; i < count.length; i++) {
-    count = new Array(lines[0].length).fill(0);
-
-    for(let line of numbers) {
-        for(let i = 0; i < line.length; i++) {
-            if(line[i] === '1') count[i]++;
-        }
-    }
-    console.log(count, i, numbers.length);
-    epsilonBin = count.map(c => c >= numbers.length / 2 ? '0' : '1').join('');
-
-    numbers = numbers.filter(value => value[i] === epsilonBin[i]);
-    if(numbers.length === 1) break;
-}
-
-let co2Bin = numbers[0];
-console.log(oxygenBin, co2Bin);
-
-let oxygen = parseInt(oxygenBin, 2);
-let co2 = parseInt(co2Bin, 2);
+let oxygen = parseInt(calcOxygenBin(lines), 2);
+let co2 = parseInt(calcCo2Bin(lines), 2);
 
 console.log("Part 2: ", oxygen * co2);
+
+function calcOxygenBin(items) {
+    return calcFilterBin(items, calcGammaBin);
+}
+
+function calcCo2Bin(items) {
+    return calcFilterBin(items, calcEpsilonBin);
+}
+
+function calcFilterBin(items, filter) {
+    let filteredItems = items.slice();
+
+    for (let i = 0; filteredItems.length > 1 && i < filteredItems[0].length; i++) {
+        let epsilonBin = filter(filteredItems);
+        filteredItems = filteredItems.filter(item => item[i] === epsilonBin[i]);
+    }
+
+    return filteredItems[0];
+}
